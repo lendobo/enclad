ff86f3f3005ecd48812943fa0a0b94aa5174991a
Marcello <marcelloibz@gmail.com>
2023-10-17 17:55:55 +0200

    backup push

--- phase_1_code/Networks/tests/gradient_check.py
+++ phase_1_code/Networks/tests/gradient_check.py
@@ -4,12 +4,20 @@ from sklearn.covariance import empirical_covariance
 import networkx as nx
 from scipy.linalg import block_diag, eigh, inv
 from numpy.random import multivariate_normal
+from sklearn.covariance import empirical_covariance
+from tqdm import tqdm
 
 
-def objective(precision_vector, S, lambda_np, lambda_wp, prior_matrix):
-    precision_matrix = precision_vector.reshape((p, p))
+
+def objective(L_vector, S, lambda_np, lambda_wp, prior_matrix):
+    # Cholesky: Reconstruct the lower triangular matrix L from the vector L_vector
+    L = np.zeros((p, p))
+    L[np.tril_indices(p)] = L_vector
+    # Reconstruct the precision matrix P = LL^T
+    precision_matrix = np.dot(L, L.T)
     det_value = np.linalg.det(precision_matrix)
     if det_value <= 0 or np.isclose(det_value, 0):
+        print("Warning: Non-invertible matrix")
         return np.inf  # return a high cost for non-invertible matrix
     
     log_det = np.log(det_value)
@@ -22,7 +30,7 @@ def objective(precision_vector, S, lambda_np, lambda_wp, prior_matrix):
     penalty_np = lambda_np * np.sum(np.abs(precision_matrix[non_prior_entries]))
 
     # create a general pentalty term, which is the l1-norm of the precision matrix
-    penalty = 0.2 * np.sum(np.abs(precision_matrix))
+    penalty = 0.3 * np.sum(np.abs(precision_matrix))
 
     objective_value = base_objective + penalty # + penalty_np + penalty_wp
     return objective_value
@@ -49,11 +57,13 @@ def callback(precision_vector):
         lambda precision_vector: objective(precision_vector, S, lambda_np, lambda_wp, prior_matrix),
         precision_vector
     )
-    print(f'Numerical Gradient at iteration: {numerical_grad}')
+    # print(f'Numerical Gradient at iteration: {numerical_grad}')
 
 # PARAMETERS
+num_runs = 5
 p = 10
 n = 250
+lambda_np, lambda_wp = [1.0, 1.0]
 
 # TRUE NETWORK
 G = nx.barabasi_albert_graph(p, 1, seed=1)
@@ -91,30 +101,59 @@ for i in range(p):
             prior_matrix[j, i] = 0.9
 np.fill_diagonal(prior_matrix, 0)
 
-# DATA MATRIX
-data = multivariate_normal(mean=np.zeros(G.number_of_nodes()), cov=covariance_mat, size=n)
+failed_gradients = []
+failed_points = []
 
-# Set initial guess as the inverse of the empirical covariance matrix
-initial_precision_vector = adjusted_precision.flatten()
+for _ in tqdm(range(num_runs)):
+    # DATA MATRIX
+    data = multivariate_normal(mean=np.zeros(G.number_of_nodes()), cov=covariance_mat, size=n)
 
-# Compute the numerical gradient
-numerical_grad = numerical_gradient(
-    lambda precision_vector: objective(precision_vector, S, lambda_np, lambda_wp, prior_matrix),
-    initial_precision_vector
-)
+    S = empirical_covariance(data)
 
-##  Print numerical gradient
-# print(numerical_grad)
+    Eye = np.eye(p)
 
-# Optimization with callback to check gradient at each iteration
-result = minimize(
-    objective,
-    initial_precision_vector,
-    args=(S, lambda_np, lambda_wp, prior_matrix),
-    method='L-BFGS-B',
-    callback=callback,
-    options={'disp': True}  # Set to True to display convergence messages
-)
+    try:
+        epsilon = 1e-3
+        L_init = np.linalg.cholesky(inv(Eye + epsilon * np.eye(p)))
+    except np.linalg.LinAlgError:
+        print("Initial Guess: non-invertible matrix")
 
-# Print optimization result
-print(result)
+    # Convert L_init to a vector representing its unique elements
+    initial_L_vector = L_init[np.tril_indices(p)]
+
+    # Compute the numerical gradient
+    numerical_grad = numerical_gradient(
+        lambda precision_vector: objective(precision_vector, S, lambda_np, lambda_wp, prior_matrix),
+        initial_L_vector
+    )
+
+    ##  Print numerical gradient
+    # print(numerical_grad)
+
+    # Optimization with callback to check gradient at each iteration
+    result = minimize(
+        objective,
+        initial_L_vector,
+        args=(S, lambda_np, lambda_wp, prior_matrix),
+        method='L-BFGS-B',
+        callback=callback,
+        options={'disp': True}  # Set to True to display convergence messages
+    )
+
+    # Check for ABNORMAL_TERMINATION_IN_LNSRCH
+    if not result.success and 'ABNORMAL_TERMINATION_IN_LNSRCH' in result.message:
+        # Compute the gradient at the termination point
+        grad_at_failure = numerical_gradient(
+            lambda precision_vector: objective(precision_vector, S, lambda_np, lambda_wp, prior_matrix),
+            result.x
+        )
+        
+        # Store the gradient and the termination point
+        failed_gradients.append(grad_at_failure)
+        failed_points.append(result.x)
+        # print the function value F of result
+
+print(len(failed_gradients), len(failed_points))  # Number of failed runs
+
+# for fail in failed_gradients:
+#     print(fail)
